607bdbad10e8de5c873dad0d73b8f9a941beff62
Estefan Antelo Doval <estefan.antelo@isolutions.ch>
2017-09-08 00:06:10 +0200

    mediaDevices is readonly, therefore we can't just reassign if already exists

--- index.js
+++ index.js
@@ -1,25 +1,22 @@
-import {SpeechRecognition} from './js/src/SpeechRecognition'
-
-if(Promise === undefined){
+import { SpeechRecognition } from './js/src/SpeechRecognition';
+if (Promise === undefined) {
     throw new Error('Promises are not supported in this browser. Use this polyfill: https://www.npmjs.com/package/es6-promise');
 }
 // polyfill mediaDevices.getUserMedia
-navigator.mediaDevices = navigator.mediaDevices || {};
-navigator.mediaDevices.getUserMedia = navigator.mediaDevices.getUserMedia || (function(){
+if (!navigator.mediaDevices) navigator.mediaDevices = {};
+navigator.mediaDevices.getUserMedia = navigator.mediaDevices.getUserMedia || (function () {
     // returns a getUserMedia function
     var getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
-    return function(constraints){
-        if(!getUserMedia){
+    return function (constraints) {
+        if (!getUserMedia) {
             return Promise.reject(new Error('getUserMedia is not implemented in this browser'));
         }
-
-        return new Promise(function(resolve, reject){
+        return new Promise(function (resolve, reject) {
             getUserMedia.call(navigator, constraints, resolve, reject);
         });
-    }
+    };
 })();
-
 // polyfill speechRecognition
-(function(speechRecognition){
-    window.SpeechRecognition = window.SpeechRecognition || SpeechRecognition
+(function (speechRecognition) {
+    window.SpeechRecognition = window.SpeechRecognition || SpeechRecognition;
 })(window.SpeechRecognition || window.webkitSpeechRecognition);
