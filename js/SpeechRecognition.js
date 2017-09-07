142279cf1ba08a5f8c7d1eff819844951d6cc40e
Estefan Antelo Doval <estefan.antelo@isolutions.ch>
2017-09-08 00:07:18 +0200

    removed unused files

--- js/SpeechRecognition.js
+++
@@ -1,8 +0,0 @@
-var SpeechRecognition = (function () {
-    function SpeechRecognition(apiKey) {
-        this.apiKey = apiKey;
-        this.continuous = false;
-    }
-    return SpeechRecognition;
-}());
-export { SpeechRecognition };
