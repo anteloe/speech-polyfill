4de74a01933a361fb2875cdb74378c004caf2a2e
Estefan Antelo Doval <estefan.antelo@isolutions.ch>
2017-09-08 00:34:28 +0200

    added recognition mechanics

--- src/SpeechRecognition.ts
+++ src/SpeechRecognition.ts
@@ -3,19 +3,66 @@ import { ISpeechGrammarList } from "./contracts/ISpeechGrammarList";
 import { resolveLang } from "./helpers/LanguageResolver";
 
 // include the needed parts of the library. webpack will treeshake all unneeded stuff.
-// import {  } from "microsoft-speech-browser-sdk/src/sdk/speech/Exports";
-// import {  } from "microsoft-speech-browser-sdk/src/sdk/speech.browser/Exports";
+import { 
+    CognitiveSubscriptionKeyAuthentication,
+    Context,
+    Device,
+    OS,
+    Recognizer,
+    RecognizerConfig,
+    RecognitionMode,
+    SpeechConfig,
+    SpeechResultFormat
+ } from "microsoft-speech-browser-sdk/src/sdk/speech/Exports";
+import { CreateRecognizer } from "microsoft-speech-browser-sdk/src/sdk/speech.browser/Exports";
 
 export class SpeechRecognition implements ISpeechRecognition{
+    private recognizer: Recognizer = null;
+
+    constructor(private apiKey: string) {}
 
     start(): void {
-        throw new Error("Method not implemented.");
+        if(!this.recognizer){
+            if(!this.apiKey){
+                throw new Error('no api key specified');
+            }
+            this.recognizer = this.setupRecognizer();
+        }
+        this.recognizer.Recognize(this.handleEvent)
+            .On(this.recognitionStartSuccess, this.recognitionStartFailed);
     }
     stop(): void {
-        throw new Error("Method not implemented.");
+        if(this.recognizer){
+            this.recognizer.AudioSource.TurnOff();
+        }
     }
     abort(): void {
-        throw new Error("Method not implemented.");
+        if(this.recognizer){
+            this.recognizer.AudioSource.TurnOff();
+        }
+    }
+
+    private handleEvent(event){
+        console.log('recognition event', event);
+    }
+
+    private recognitionStartSuccess(listening: boolean){
+        console.log('recognition started');
+    }
+
+    private recognitionStartFailed(error){
+        console.log('recognition start failed', error)
+    }
+
+    private setupRecognizer(){
+        const speechConfig = new SpeechConfig(new Context(
+            new OS('Speech', 'Speech', null),
+            new Device(navigator.userAgent, 'Browser', '1.0.0.0')
+        ));
+        const config = new RecognizerConfig(speechConfig, RecognitionMode.Dictation, resolveLang(this.lang), SpeechResultFormat.Detailed);
+        const auth = new CognitiveSubscriptionKeyAuthentication(this.apiKey);
+
+        return CreateRecognizer(config, auth);
     }
 
     onaudiostart: () => void = null;
