25473aac47760a862e3489a1cad89a3b72045461
Estefan Antelo Doval <estefan.antelo@isolutions.ch>
2017-09-07 11:24:30 +0200

    added a language to locale resolver helper

---
+++ src/helpers/LanguageResolver.ts
@@ -0,0 +1,25 @@
+const supportedDefaults = {
+    ar: 'ar-EG',
+    ca: 'ca-ES',
+    da: 'da-DK',
+    de: 'de-DE',
+    en: 'en-US',
+    es: 'es-ES',
+    fi: 'fi-FI',
+    fr: 'fr-FR',
+    hi: 'hi-IN',
+    it: 'it-IT',
+    ja: 'ja-JP',
+    ko: 'ko-KR',
+    nb: 'nb-NO',
+    nl: 'nl-NL',
+    pl: 'pl-PL',
+    pt: 'pt-PT',
+    ru: 'ru-RU',
+    sv: 'sv-SE',
+    zh: 'zh-CN'
+}
+
+export const resolveLang: (string) => string = (langHint: string) => {
+    return supportedDefaults[langHint] || langHint;
+}
