29cc279c515d6e867aa7b5b9e6dba65bf38d37f6
Estefan Antelo Doval <estefan.antelo@isolutions.ch>
2017-09-09 18:28:33 +0200

    Added Congitive Services speech result interface

--- src/contracts.ts
+++ src/contracts.ts
@@ -4,3 +4,4 @@ export { ISpeechRecognition } from "./contracts/ISpeechRecognition";
 export { ISpeechRecoginitionAlternative } from "./contracts/ISpeechRecognitionAlternative";
 export { ISpeechRecoginitionResult } from "./contracts/ISpeechRecoginitionResult";
 export { ISpeechRecognitionResultList } from "./contracts/ISpeechRecognitionResultList";
+export { ICoginitiveServiceSpeechResult } from './contracts/ICoginitiveServiceSpeechResult'
---
+++ src/contracts/ICoginitiveServiceSpeechResult.ts
@@ -0,0 +1,7 @@
+export interface ICoginitiveServiceSpeechResult{
+    Confidence: number;
+    Display: string;
+    ITN: string;
+    Lexical: string;
+    MaskedITN: string;
+}
